3a872529fd5756890e7abef2b80e6205724d894e
deko81 <deko81@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-29 06:28:09 +0000

    re #2 start parsing //TODO: parse states first & last.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <string.h>
+#include <string>
+//#include <fstream>
+#include "nfa.h"
+
 
 using namespace std;
 
@@ -10,9 +13,7 @@ int main(int argc, char *argv[])
     bool flag=false;
     char *file_name;
 	//simulating parameters
-	//argc=2;
-	
-	
+	file_name="teste.txt";/*
 	//parsing parameters
     if (argc>1){
         bool is_ok=true;
@@ -24,19 +25,18 @@ int main(int argc, char *argv[])
                 param=aux[1];
             else {is_ok=false;}
             switch(param){
-                case 'f':          if((!strcmp(aux,"-f"))&&argc>(j+1)){
-                                   flag=1;
-                                   file_name=argv[++j];
-                                   cout<<file_name<<endl;
-                                   break;
-                                   }
+                case 'f':			if((!strcmp(aux,"-f"))&&argc>(j+1)){
+										flag=1;
+										file_name=argv[++j];
+										break;
+									}
 
-                case 'h':          cout<< "-h\n"<<usage;
-                default:           char msg[]=": wrong parameter\n";
-                                   char tmp[strlen(msg)+strlen(usage)+2];
-                                   strcat(tmp,aux);
-                                   strcat(tmp,msg);
-                                   cout << tmp;
+                case 'h':			cout<< "-h\n"<<usage;
+                default:			char msg[]=": wrong parameter\n";
+									char tmp[strlen(msg)+strlen(usage)+2];
+									strcat(tmp,aux);
+									strcat(tmp,msg);
+									cout << tmp;
 
             }
             if (j<(argc-1))
@@ -47,8 +47,30 @@ int main(int argc, char *argv[])
     if (!flag){
         cout << usage << endl;
         return 0;
-    }
+    }*/
 	//Put your code here 
-    cout << "\nExecutando programa!\n"<<endl;
-    return 0;
+    //cout << "\nExecutando programa!\n"<<endl;
+    cout<<"Abrindo "<<file_name<<"!"<<endl;
+	/*ofstream fresult;
+	fresult.open("result.txt", ios::out);
+	ifstream infile (file_name, ios::in);
+	char myline[256];
+	int lc=0;
+	if (! infile) {
+		cerr << "Failed to open input file\n";
+		exit(1);
+	}
+	while (1) {
+		infile.getline(myline,256);
+		if (infile.eof())
+			break;
+		lc++;
+		fresult << lc << ": "<<myline<<" \n";
+	}
+	infile.close();
+	fresult.close();*/
+	Cnfa my_nfa;
+	my_nfa.set_file(file_name);
+	my_nfa.~Cnfa();
+	return 0;
 }
