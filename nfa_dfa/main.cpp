79329aec774edcbd99812d76f68057b66d4c2d27
deko81 <deko81@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-31 21:04:24 +0000

    r#6 minnor changes/fixes

--- nfa_dfa/main.cpp
+++ nfa_dfa/main.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include <iostream>
 #include <string>
 //#include <fstream>
@@ -9,13 +11,14 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     char usage[]=
-    "Compilador version 0.001ALPHA1\t    Copyright (c) 2009 Andre Luis da Cunha\n\nUsage:     compilador <command>\n<Commands>\n-f <file>\t     Open <file>";
+    "Compilador version 0.001ALPHA1\t    Copyright (c) 2009 Andre Luis da Cunha\n\nUsage:     compilador <command>\n<Commands>\n-f <file>\t     Open <file>\n";
     bool flag=false;
     char *file_name;
 	//simulating parameters
 	file_name="teste.txt";/*
 	//parsing parameters
-    if (argc>1){
+    
+	if (argc>1){
         bool is_ok=true;
         while(is_ok){
             int j=1;
@@ -32,6 +35,8 @@ int main(int argc, char *argv[])
 									}
 
                 case 'h':			cout<< "-h\n"<<usage;
+									break;
+				
                 default:			char msg[]=": wrong parameter\n";
 									char tmp[strlen(msg)+strlen(usage)+2];
 									strcat(tmp,aux);
@@ -47,30 +52,13 @@ int main(int argc, char *argv[])
     if (!flag){
         cout << usage << endl;
         return 0;
-    }*/
-	//Put your code here 
+    }//*/
+	//Put your code here
     //cout << "\nExecutando programa!\n"<<endl;
-    cout<<"Abrindo "<<file_name<<"!"<<endl;
-	/*ofstream fresult;
-	fresult.open("result.txt", ios::out);
-	ifstream infile (file_name, ios::in);
-	char myline[256];
-	int lc=0;
-	if (! infile) {
-		cerr << "Failed to open input file\n";
-		exit(1);
-	}
-	while (1) {
-		infile.getline(myline,256);
-		if (infile.eof())
-			break;
-		lc++;
-		fresult << lc << ": "<<myline<<" \n";
-	}
-	infile.close();
-	fresult.close();*/
+    //cout<<"Abrindo "<<file_name<<"!"<<endl;
+	
 	Cnfa my_nfa;
 	my_nfa.set_file(file_name);
-	my_nfa.~Cnfa();
+	my_nfa.~Cnfa();/**/
 	return 0;
 }
